0110aee39c428bf9f8776b379844d330a7b54fe9
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 14:22:59 -0300

    update allLongestStrings.js

--- src/allLongestStrings.js
+++ src/allLongestStrings.js
@@ -16,3 +16,10 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test allLongestStrings
+
+// alternative solution
+// function solution(inputArray) {
+//     'use strict';
+//     let maxSize = Math.max(...inputArray.map(x => x.length));
+//     return inputArray.filter(x => x.length === maxSize);
+// }
